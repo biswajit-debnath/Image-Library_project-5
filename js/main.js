5dac3cfb85345f4ef078db688f86fbc43bab478a
sumit-001 <sumitnath001@outlook.com>
2019-08-26 19:32:38 +0530

    Completed
    A beautiful design layout i made

--- js/main.js
+++ js/main.js
@@ -1,6 +1,24 @@
 lightbox.option({
-    'resizeDuration': 200,
-    'wrapAround': false,
-    'fitImagesInViewport': true
+  'showImageNumberLabel': false,
   })
- 
+let arr = [
+  'bluebells',
+  'canyon',
+  'cave',
+  'countryside',
+  'desert',
+  'dunes',
+  'fall',
+  'hay',
+  'iceberg',
+  'lake',
+  'plantation',
+  'sunset'
+];
+
+function myFunction() {
+  let $text = $('.search_box').val();
+  $text = $text.toLowerCase();
+  for(let i=0;i<arr.length;i++)
+    $('a').eq(i).toggle(arr[i].indexOf($text)>-1);
+}
